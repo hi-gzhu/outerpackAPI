5f891d09d445498a18995964b267ed41a78f230d
dateolive <2448282543@qq.com>
2021-03-28 22:33:34 +0800

    push test

--- src/test/java/com/outerpack/OuterpackApplicationTests.java
+++ src/test/java/com/outerpack/OuterpackApplicationTests.java
@@ -1,13 +1,22 @@
 package com.outerpack;
 
+import com.outerpack.entity.pojo.Manager;
+import com.outerpack.service.MangerService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OuterpackApplicationTests {
 
+    @Autowired
+    MangerService mangerService;
+
     @Test
     void contextLoads() {
+
+        Manager managerById = mangerService.getManagerById(1);
+        System.out.println(managerById);
     }
 
 }
