dce7098c71c91a54c5ca77018b5ee4a4aaf69d85
dateolive <2448282543@qq.com>
2021-03-30 15:28:06 +0800

    :art: 添加注释

--- src/main/java/com/outerpack/mapper/ManagerMapper.java
+++ src/main/java/com/outerpack/mapper/ManagerMapper.java
@@ -12,9 +12,25 @@ public interface ManagerMapper {
 //    Manager getManagerById(Integer id);
 //    int changePassword(String username,String password);
 
+    /**
+     * 根据管理员账户名获取管理员信息
+     * @param username 管理员账户名
+     * @return
+     */
     Manager getManagerByName(@Param("username") String username);
 
+    /**
+     * 根据管理员id获取管理员信息
+     * @param id
+     * @return
+     */
     Manager getManagerById(@Param("ID") Integer id);
 
+    /**
+     * 根据管理员账户名和密码修改账户密码
+     * @param username
+     * @param password
+     * @return
+     */
     int changePassword(@Param("username") String username,@Param("password") String password);
 }
